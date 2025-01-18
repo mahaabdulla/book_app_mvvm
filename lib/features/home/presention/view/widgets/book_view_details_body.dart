4a39e3b823289f7423084337f594cb094d3262bf
mahaabdulla <mahaabdulla87@gmail.com>
2025-01-18 03:13:02 +0300

    add text to details book view

--- lib/features/home/presention/view/widgets/book_view_details_body.dart
+++ lib/features/home/presention/view/widgets/book_view_details_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presention/view/widgets/custome_appbar_books_detils.dart';
 import 'package:bookly_app/features/home/presention/view/widgets/custome_Book_images.dart';
 import 'package:flutter/material.dart';
@@ -13,14 +14,21 @@ class BookViewDetailsBody extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 30.0),
+          padding: const EdgeInsets.symmetric(horizontal: 30.0),
           child: Column(
             children: [
               const CustomeAppBarBooksDetails(),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.12),
-                child: CustomBookImage(),
+                child: const CustomBookImage(),
               ),
+              const SizedBox(height: 20),
+              const Text(
+                'The Jungle Book',
+                style: Styles.textStyle20,
+              ),
+              const SizedBox(height: 10),
+              const Opacity(opacity: 0.7, child: Text('The Jungle Book')),
             ],
           ),
         ),
